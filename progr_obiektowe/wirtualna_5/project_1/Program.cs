b652396fd313dc548c314c4d5cad8709ecefcc4e
szyper <31780468+szyper@users.noreply.github.com>
2025-09-27 20:23:17 +0200

    project car

--- progr_obiektowe/wirtualna_5/project_1/Program.cs
+++ progr_obiektowe/wirtualna_5/project_1/Program.cs
@@ -8,8 +8,95 @@ namespace project_1
 {
   internal class Program
   {
+    public enum Color
+    {
+      Czerwony,
+      Zielony,
+      Niebieski
+    }
+
+    public enum Type
+    {
+      Samochód,
+      Motor,
+      Ciężarówka,
+      Suv
+    }
+
+    class Vehicle
+    {
+      public string Name { get; set; }
+      public Color Color { get; set; }
+      public double Speed { get; set; }
+      public double Fuel { get; set; }
+      public List<Type> Type { get; set; }
+      public string Extra { get; set; }
+
+      public void ShowData()
+      {
+        Console.WriteLine($"Nazwa: {Name}, kolor: {Color}, prędkość: {Speed}, rodzaj paliwa: {Fuel}, Typ: {string.Join(", ", Type)}.\nDodatkowe informacje: {Extra}");
+      }
+    }
+
+    class Garage
+    {
+      public List<Vehicle> Vehicles { get; set; }
+      public byte Capacity { get; set; }
+      public void AddVehicles(Vehicle vehicle)
+      {
+        if (Vehicles.Count < Capacity)
+        {
+          Vehicles.Add(vehicle);
+          Console.WriteLine($"Dodano {vehicle.Name} do garażu");
+        }
+        else
+        {
+          Console.WriteLine("Brak miejsc w garażu");
+        }
+      }
+
+      public void RemoveVehicle(Vehicle vehicle)
+      {
+        if (Vehicles.Contains(vehicle))
+        {
+          Vehicles.Remove(vehicle);
+          Console.WriteLine($"Usunięto {vehicle.Name} z garażu");
+        }
+        else
+        {
+          Console.WriteLine("Brak pojazdu w garażu");
+        }
+      }
+    }
+
     static void Main(string[] args)
     {
+      Vehicle bike1 = new Vehicle();
+      bike1.Name = "Yamaha";
+      bike1.Type = new List<Type> { Type.Motor }; 
+      Console.WriteLine(bike1.Name);
+      bike1.ShowData();
+      Console.Clear();
+
+      Vehicle car1 = new Vehicle() { Name = "Fiat", Color = Color.Niebieski, Type = new List<Type> { Type.Samochód, Type.Suv}, Speed = 115, Fuel = 50, Extra = "126p" };
+      car1.ShowData();
+
+      Garage garage = new Garage();
+      garage.Capacity = 2;
+
+      garage.Vehicles = new List<Vehicle>();
+
+      //garage.AddVehicles(bike1);
+      garage.AddVehicles(car1);
+      garage.AddVehicles(car1);
+
+      Vehicle car2 = new Vehicle() { Name = "Fiat", Color = Color.Zielony, Type = new List<Type> { Type.Samochód, Type.Suv }, Speed = 115, Fuel = 50, Extra = "126p" };
+      garage.AddVehicles(car2);
+
+      garage.RemoveVehicle(car1);
+      garage.AddVehicles(car2);
+
+
     }
   }
 }
