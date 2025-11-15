7e0a495d01fa90a15fd30bb2287276d93d5188d8
szyper <31780468+szyper@users.noreply.github.com>
2025-11-15 18:42:51 +0100

    c#

---
+++ progr_obiektowe/wyklad/konstruktory_shallow_deep_copy/Program.cs
@@ -0,0 +1,150 @@
+﻿namespace konstruktory_shallow_deep_copy
+{
+  internal class Program
+  {
+    public class Engine
+    {
+      public int HorsePower { get; set; }
+      public double Capacity { get; set; }
+      
+      // pole statyczne - wspólne dla wszystkich obiektów Engine
+      public static int EngineCount;
+      
+      // konstruktor statyczny
+      static Engine()
+      {
+        EngineCount = 0;
+        Console.WriteLine("Statyczny konstruktor Engine został wywołany");
+      }
+
+      public Engine(int horsePower, double capacity)
+      {
+        HorsePower = horsePower;
+        Capacity = capacity;
+        EngineCount++;
+      }
+
+      public Engine(Engine other)
+      {
+        HorsePower = other.HorsePower;
+        Capacity = other.Capacity;
+        EngineCount++;
+      }
+
+      ~Engine()
+      {
+        Console.WriteLine("Destruktor silnika został wywołany");
+        EngineCount--;
+      }
+      
+      public override string ToString()
+      {
+        return $"Moc silnika: {HorsePower}HP, {Capacity}L, liczba silników: {EngineCount}";
+      }
+    }
+    
+    public class Car
+    {
+      public string Brand { get; set; }
+      public Engine Engine { get; set; }
+      public static int CarCount;
+
+      static Car()
+      {
+        CarCount = 0;
+        Console.WriteLine("Statyczny konstruktor Car został wywołany");
+      }
+
+      public Car(string brand, Engine engine)
+      {
+        Brand = brand;
+        Engine = engine;
+        CarCount++;
+      }
+
+      public Car(Car other)
+      {
+        Brand = other.Brand;
+        Engine = new Engine(other.Engine);
+        CarCount++;
+      }
+
+      ~Car()
+      {
+        Console.WriteLine("Destruktor samochodu został wywołany");
+        CarCount--;
+      }
+      
+      public override string ToString()
+      {
+        return $"Marka samochodu: {Brand}, {Engine}, ilość samochodów: {CarCount}";
+      }
+    }
+
+    public class ElectricCar : Car
+    {
+      public int BatteryCapacity { get; set; }
+
+      public enum DrivingMode { Eco, Normal, Sport }
+      public DrivingMode Mode { get; private set; } = DrivingMode.Normal;
+
+      public ElectricCar(string brand, Engine engine, int batteryCapacity) : base(brand, engine)
+      {
+        BatteryCapacity = batteryCapacity;
+      }
+
+      public ElectricCar(ElectricCar other) : base(other)
+      {
+        BatteryCapacity = other.BatteryCapacity;
+        Mode = other.Mode;
+      }
+
+      ~ElectricCar()
+      {
+        Console.WriteLine("Destruktor samochodu elektrycznego został wywołany");
+      }
+      
+      public override string ToString()
+      {
+        return $"Samochód elektyczny: {Brand}, {Engine}, bateria: {
+        BatteryCapacity}kWh, tryb jazdy: {Mode}";
+      }
+    }
+    static void Main(string[] args)
+    {
+      Car car1 = new Car("Audi", new Engine(400, 2.0));
+      Car car2 = new Car(car1);
+
+      car2.Engine.HorsePower = 500;
+
+      Console.WriteLine($"car1: {car1.Brand}, {car1.Engine.HorsePower}HP");
+      Console.WriteLine($"car2: {car2.Brand}, {car2.Engine.HorsePower}HP");
+      
+      car1.Engine.HorsePower = 1000;
+      Console.WriteLine($"car1: {car1.Brand}, {car1.Engine.HorsePower}HP");
+      Console.WriteLine($"car2: {car2.Brand}, {car2.Engine.HorsePower}HP");
+
+      Console.WriteLine("====================");
+      Console.WriteLine(car1);
+      Console.WriteLine("====================");
+
+      ElectricCar eCar1 = new ElectricCar("Tesla", new Engine(400, 0), 100);
+
+      ElectricCar eCar2 = new ElectricCar(eCar1);
+      eCar2.Engine.HorsePower = 222;
+
+      Console.WriteLine(car1);
+      Console.WriteLine(eCar1);
+      Console.WriteLine(eCar2);
+
+      car1 = null;
+      eCar1 = null;
+      eCar2 = null;
+      
+      GC.Collect();
+      GC.WaitForPendingFinalizers();
+
+      Console.WriteLine("Koniec programu");
+    }
+  }
+}
---
+++ progr_obiektowe/wyklad/wyklad_w_konstruktory_destruktory/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace wyklad_w_konstruktory_destruktory
+{
+  internal class Program
+  {
+    class Person
+    {
+      public string name;
+      public int age;
+
+      public Person()
+      {
+        name = "Nieznane";
+        age = 18;
+      }
+
+      public Person(string name)
+      {
+        this.name = name;
+      }
+    }
+
+    class Student
+    {
+      public string firstName;
+      public string studentId;
+
+      public Student()
+      {
+        Console.Write("Podaj swoje imię: ");
+        firstName = Console.ReadLine();
+
+      }
+
+      public Student(string FirstName, string StudentId)
+      {
+        firstName = FirstName;
+        studentId = StudentId;
+      }
+    }
+
+    class Car
+    {
+      public string brand;
+      public string model;
+      public int year;
+
+      public Car(string brand)
+      {
+        this.brand = brand;
+      }
+
+      public Car(string brand, string model, int year)
+      {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+      }
+
+      public Car(Car other)
+      {
+        brand = other.brand;
+        model = other.model;
+        year = other.year;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Person p = new Person();
+      Console.WriteLine(p.age); //0
+      Console.WriteLine(p.name);
+
+      Person p1 = new Person("Jan");
+      Console.WriteLine(p1.name);
+
+      Student s = new Student();
+      Console.WriteLine(s.firstName);
+
+      Console.Write("Podaj swoje imię: ");
+      string name = Console.ReadLine();
+
+      Console.Write("Podaj id: ");
+      string id = Console.ReadLine();
+
+      Student s2 = new Student(name, id);
+      Console.WriteLine($"Imię: {s2.firstName}, id: {s2.studentId}");
+
+      Car c = new Car("VW");
+      Console.WriteLine();
+
+      Car c3 = new Car("Fiat", "126p", 1984);
+      Console.WriteLine($"Marka: {c3.brand}, model: {c3.model}, rok produkcji: {c3.year}");
+      Car cn = new Car(c3);
+      Console.WriteLine($"Marka: {cn.brand}, model: {cn.model}, rok produkcji: {cn.year}");
+
+      c3.brand = "Ferrari";
+      Console.WriteLine(cn.brand); //Fiat
+      cn.brand = "Porsche";
+      Console.WriteLine(cn.brand); //Porsche
+      Console.WriteLine(c3.brand); //Ferrari
+
+
+
+
+    }
+  }
+}
